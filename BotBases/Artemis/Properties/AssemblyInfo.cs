e4af8926fbf8b840d5d14d7f2dcdea4a4545775e
sodimm <andy.beynon@outlook.com>
2017-04-30 12:09:42 +0100

    Updated Artemis to v2.0.0
    New WPF Interface: 
    Dark/Crimson metro styling.
    Enemy names will now adjust to client language.
    As well as AutoComplete, ComboBox and IntegerUpDown boxes will now capture mouse wheel input.
    Settings *.json moved to it's own /Artemis/ folder within Character settings instead of Global Settings.
     
    Improved Combat Targeting:
    Bot no long tries to continue with another task before killing all attackers (Making it stand still and do nothing).
    
    Rewrites and tweaks to Logic:
    Bot no longer throws a ReadProcess error on completion.
    Bot no longer stands and does nothing after Death and Revive.

--- BotBases/Artemis/Properties/AssemblyInfo.cs
+++ BotBases/Artemis/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Loader.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]

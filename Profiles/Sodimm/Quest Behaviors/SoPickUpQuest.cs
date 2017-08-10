fd06575ff3a3c0752cc3ce384c762c8b1e1288ac
sodimm <andy.beynon@outlook.com>
2017-08-10 23:20:22 +0100

    Added Behavior: SoPickUpQuest.cs Amended Profile: [OrderBot] Paladin 60 - 70.xml to use SoPickUpQuest for Dialog Options.

---
+++ Profiles/Sodimm/Quest Behaviors/SoPickUpQuest.cs
@@ -0,0 +1,31 @@
+﻿using Clio.XmlEngine;
+using ff14bot.RemoteWindows;
+using System.ComponentModel;
+using TreeSharp;
+using Action = TreeSharp.Action;
+
+namespace ff14bot.NeoProfiles.Tags
+{
+    [XmlElement("SoPickUpQuest")]
+    [XmlElement("SoPickupQuest")]
+
+    class SoPickUpQuest : PickupQuestTag
+    {
+        [DefaultValue(0)]
+        [XmlAttribute("DialogOption")]
+        public int DialogOption { get; set; }
+
+        protected override Composite CreateBehavior()
+        {
+            return new PrioritySelector(
+                new Decorator(ret => SelectString.IsOpen,
+                    new Action(r =>
+                    {
+                        SelectString.ClickSlot((uint)DialogOption);
+                    })
+                ),
+                base.CreateBehavior()
+            );
+        }
+    }
+}
